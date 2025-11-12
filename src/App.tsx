906739915bce921e5b69b22ce549cded38d96092
ArrMannDev <arrmann632025@gmail.com>
2025-11-12 08:56:07 +0630

    add nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import BeamGridBackground from './components/lightswind/beam-grid-background'
+import BeamGridBackground from "./components/lightswind/beam-grid-background";
+import Nav from "./components/nav";
 
 function App() {
-
   return (
     <>
-      <BeamGridBackground 
-      gridSize={30}
-      gridColor="#d1d5db"
-      darkGridColor="#1f2937"
-      beamColor="#000000"
-      darkBeamColor="ffffff"
-      beamCount={10}
-      extraBeamCount={3}
-      beamThickness={3}
-      beamGlow
-      glowIntensity={50}
-      idleSpeed={1.15}
-      asBackground={true}
+      <BeamGridBackground
+        gridSize={30}
+        gridColor="#d1d5db"
+        darkGridColor="#1f2937"
+        beamColor="rgba(0,180,255,0.8)"
+        darkBeamColor="rgba(0,255,255,0.8)"
+        beamCount={10}
+        extraBeamCount={3}
+        beamThickness={3}
+        beamGlow
+        glowIntensity={50}
+        idleSpeed={1.15}
+        asBackground={true}
       />
+      <Nav />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,5 +1,22 @@
-function Nav(){
+import { ToggleTheme } from "./lightswind/toggle-theme";
 
+function Nav(){
+    return(
+        <div className="w-full h-auto fixed flex flex-row items-center justify-between p-3 bg-transparent">
+            <h1>Logo</h1>
+
+            <nav>
+                <ul className="flex flex-row gap-5">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Project</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Certificate</a></li>
+                </ul>
+            </nav>
+
+            <ToggleTheme />
+        </div>
+    );
 }
 
 export default Nav
